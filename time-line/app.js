9c9b181e9d4e222b88902ee8273148a54569eaba
Mmmad Amin Mahjub Asl <aminmahjoob3172@gmail.com>
2023-04-01 14:35:38 +0330

    Finish timeLine project

--- time-line/app.js
+++ time-line/app.js
@@ -1,5 +1,9 @@
-const stations = document.querySelectorAll('.station');
+const stations = document.querySelectorAll('.stations');
 const buttons = document.querySelectorAll('.button-wrapper button');
+const timeLine = document.querySelector('.time-line');
+let timeLineWidth = 0;  
+
+console.log(timeLine);
 let score = 0;
 
 const [privously, next] = buttons;
@@ -13,24 +17,32 @@ next.addEventListener('click', (event) => {
 });
 
 function changeTimeLine(event) {
-    
     if (event.target.classList[0] === 'previuosly-btn') {
         score--;
         stations[score].classList.add('stayed-station');
-        stations[score + 1].classList.remove('stayed-station');
-
-        next.style.cursor = 'pointer';
-        next.disabled = false;
+        stations[score + 1].classList.remove('active');
+        timeLineWidth > 0 ? timeLineWidth -= 25 : timeLineWidth = 0;  
     } else {
         score++;
-        stations[score].classList.add('stayed-station');
-
-        privously.style.cursor = 'pointer';
-        privously.disabled = false;
+        stations[score].classList.add('active');
+        timeLineWidth < 100 ? timeLineWidth += 25 : timeLineWidth = 100;  
     }
 
-    if (score === 4 || score === 0) {
-        event.target.style.cursor = 'not-allowed';
-        event.target.disabled = true;
+    timeLine.style.width = `${timeLineWidth}%`;
+    
+    if (stations[stations.length - 1].classList[1] === 'active') {
+        next.classList.add('disabled-btn');
+        next.disabled = true;
+    } else {
+        next.classList.remove('disabled-btn');
+        next.disabled = false;
+    }
+
+    if (stations[0].classList[1] === 'active' && score === 0) {
+        privously.classList.add('disabled-btn');
+        privously.disabled = true;
+    } else {
+        privously.classList.remove('disabled-btn');
+        privously.disabled = false;
     }
 }
